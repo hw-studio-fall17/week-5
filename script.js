0015976d3082b3469af18ac219afa7d763d9f75c
Ian Cullicott <ian.cullicott6e@stu.austinisd.org>
2017-10-17 17:01:57 -0600

    jquery

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 
-
-
-
-
+var words = $(".word");
+words.fadeOut();
+var we = $("#we")
+we.fadeIn("slow");
+var love = $("#love")
+love.fadeIn("fast");
+var to = $("#to")
+to.fadeIn("slow");
+var code = $("#code")
+code.fadeIn("fast");
 })
