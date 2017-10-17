7c22a672ae23e6ccab996caa296b22d7662ed314
Jason Agnew <jason.agnew7o@stu.austinisd.org>
2017-10-17 18:01:44 -0500

    Jquery

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
-
-
-
+var words = $(".word")
+words.fadeOut("fast")
+var we = $("#we")
+we.fadeIn("slow")
+var love = $("#love")
+love.fadeIn("fast")
+var to = $("#to")
+to.fadeIn("slow")
+var code = $("#code")
+code.fadeIn("slow")
 
 })
